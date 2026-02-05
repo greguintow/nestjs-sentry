1110d1c808acc35020c5a5419e94665ee6bd6de1
Greg Teixeira <greg.teixeira123@gmail.com>
2026-02-05 10:59:14 -0300

    chore: update version to 4.1.9 and add @nestjs/graphql as a dev dependency

--- lib/sentry.module.ts
+++ lib/sentry.module.ts
@@ -1,17 +1,10 @@
 import { Module, DynamicModule, MiddlewareConsumer } from '@nestjs/common'
-import { HttpAdapterHost, ModuleRef } from '@nestjs/core'
 import { SentryCoreModule } from './sentry-core.module'
 import { SentryModuleOptions, SentryModuleAsyncOptions } from './sentry.interfaces'
 import { SentryRequestMiddleware } from './sentry-request.middleware'
-import { SentryTracingMiddleware } from './sentry-tracing.middleware'
 
 @Module({})
 export class SentryModule {
-  constructor(
-    private readonly httpAdapterHost: HttpAdapterHost,
-    private readonly moduleRef: ModuleRef
-  ) {}
-
   public static forRoot(options: SentryModuleOptions): DynamicModule {
     return {
       module: SentryModule,
@@ -27,17 +20,6 @@ export class SentryModule {
   }
 
   configure(consumer: MiddlewareConsumer) {
-    const adapterName = this.httpAdapterHost.httpAdapter?.constructor?.name
-
-    if (adapterName === 'FastifyAdapter') {
-      const sentryRequestMiddleware = new SentryRequestMiddleware()
-      this.httpAdapterHost.httpAdapter
-        .getInstance()
-        .addHook('preHandler', (req: any, res: any, done: any) => {
-          sentryRequestMiddleware.use(req, res, done)
-        })
-    } else {
-      consumer.apply(SentryRequestMiddleware).forRoutes('*')
-    }
+    consumer.apply(SentryRequestMiddleware).forRoutes('*')
   }
 }
